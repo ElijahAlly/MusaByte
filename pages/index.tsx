d8503ca93b71449142a69e305d78bcf8655bda03
Elijah Ally <elijahally18@gmail.com>
2021-10-06 18:08:38 -0400

    started media queries :(

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,9 @@ import Navbar from '../components/navbar';
 const Home: NextPage = () => {
 	return (
 		<>
+			<head>
+				<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+			</head>
 			<Navbar />
 		</>
 	);
